fe04dabf204ee86bc4e4dd97794bfb001c9c29dd
Adedamola <wuraolaadedamola>
2018-09-04 15:22:30 +1200

    Second Commit
    More Examples

---
+++ lstm_PTB.py
@@ -0,0 +1,242 @@
+#http://adventuresinmachinelearning.com/keras-lstm-tutorial/
+
+from __future__ import print_function
+import collections
+import os
+import tensorflow as tf
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Activation, Embedding, Flatten, Dropout, TimeDistributed, Reshape, Lambda
+from keras.layers import LSTM
+from keras.optimizers import RMSprop, Adam, SGD
+from keras import backend as K
+import keras
+from keras.utils import to_categorical
+from keras.callbacks import ModelCheckpoint
+import numpy as np
+import argparse
+import pdb
+from keras.utils.generic_utils import get_custom_objects
+
+#reproducibility
+from keras import backend as K
+import os
+import random as rn
+os.environ['PYTHONHASHSEED'] ='0'
+#Below is necessary for starting Numpy generated 
+#random numbers in a well-defined initial state
+np.random.seed(42)
+
+#Below is necessary for starting core Python generated 
+#random numbers in a well-defined initial state
+rn.seed(12345)
+'''force tensorflow to use single thread
+   multiple threads are the potential source
+   of non-reproducible results
+'''
+session_conf = tf.ConfigProto(intra_op_parallelism_threads=1, inter_op_parallelism_threads=1)
+#The below tf.set_random_seed() will make random number generation in
+#Tensorflow backend have well defined initial state
+tf.set_random_seed(1234)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+
+
+#Define activation functions
+def tf_sqnlsig(x):
+    u=tf.clip_by_value(x,-2,2)
+    a = u
+    b= tf.negative(tf.abs(u))
+    wsq = (tf.multiply(a,b))/4.0
+    y = tf.add(tf.multiply(tf.add(u,wsq),0.5),0.5)
+    return y
+get_custom_objects().update({'custom_activation': Activation(tf_sqnlsig)})
+def tf_sqnl(x): 
+    u=tf.clip_by_value(x,-2,2)
+    a = u
+    b= tf.negative(tf.abs(u))
+    wsq = (tf.multiply(a,b))/4.0
+    y = tf.add(u,wsq)
+    return y
+get_custom_objects().update({'custom_activation': Activation(tf_sqnl)})
+
+def tf_tansig(x): 
+    w=tf.multiply(tf.negative(2.0), x)
+    u=tf.exp(w)
+    a = 1.0 + u
+    b= 2.0/a
+    y = b - 1.0
+    return y
+get_custom_objects().update({'custom_activation': Activation(tf_tansig)})
+
+def tf_logsig(x): 
+    u=tf.exp(tf.negative(x)) 
+    a = tf.add(u,1.0)
+    y= 1.0/a
+    return y
+get_custom_objects().update({'custom_activation': Activation(tf_logsig)})
+
+
+
+
+
+data_path = "/Users/adedamola/Documents/PhD/tensorflowtut/ELU_Paper/LSTM_Examples/simple-examples/data"
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--run_opt', type=int, default=1, help='An integer: 1 to train, 2 to test')
+parser.add_argument('--data_path', type=str, default=data_path, help='The full path of the training data')
+args = parser.parse_args()
+if args.data_path:
+    data_path = args.data_path
+
+def read_words(filename):
+    with tf.gfile.GFile(filename, "r") as f:
+        return f.read().replace("\n", "<eos>").split()
+
+
+def build_vocab(filename):
+    data = read_words(filename)
+
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+
+    return word_to_id
+
+
+def file_to_word_ids(filename, word_to_id):
+    data = read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+
+def load_data():
+    # get the data paths
+    train_path = os.path.join(data_path, "ptb.train.txt")
+    valid_path = os.path.join(data_path, "ptb.valid.txt")
+    test_path = os.path.join(data_path, "ptb.test.txt")
+
+    # build the complete vocabulary, then convert text data to list of integers
+    word_to_id = build_vocab(train_path)
+    train_data = file_to_word_ids(train_path, word_to_id)
+    valid_data = file_to_word_ids(valid_path, word_to_id)
+    test_data = file_to_word_ids(test_path, word_to_id)
+    vocabulary = len(word_to_id)
+    reversed_dictionary = dict(zip(word_to_id.values(), word_to_id.keys()))
+
+    #print(train_data[:5])
+    #print(word_to_id)
+    #print(vocabulary)
+    #print(" ".join([reversed_dictionary[x] for x in train_data[:10]]))
+    return train_data, valid_data, test_data, vocabulary, reversed_dictionary
+
+train_data, valid_data, test_data, vocabulary, reversed_dictionary = load_data()
+
+
+class KerasBatchGenerator(object):
+
+    def __init__(self, data, num_steps, batch_size, vocabulary, skip_step=5):
+        self.data = data
+        self.num_steps = num_steps
+        self.batch_size = batch_size
+        self.vocabulary = vocabulary
+        # this will track the progress of the batches sequentially through the
+        # data set - once the data reaches the end of the data set it will reset
+        # back to zero
+        self.current_idx = 0
+        # skip_step is the number of words which will be skipped before the next
+        # batch is skimmed from the data set
+        self.skip_step = skip_step
+
+    def generate(self):
+        x = np.zeros((self.batch_size, self.num_steps))
+        y = np.zeros((self.batch_size, self.num_steps, self.vocabulary))
+        while True:
+            for i in range(self.batch_size):
+                if self.current_idx + self.num_steps >= len(self.data):
+                    # reset the index back to the start of the data set
+                    self.current_idx = 0
+                x[i, :] = self.data[self.current_idx:self.current_idx + self.num_steps]
+                temp_y = self.data[self.current_idx + 1:self.current_idx + self.num_steps + 1]
+                # convert all of temp_y into a one hot representation
+                y[i, :, :] = to_categorical(temp_y, num_classes=self.vocabulary)
+                self.current_idx += self.skip_step
+            yield x, y
+
+num_steps = 30
+batch_size = 20
+train_data_generator = KerasBatchGenerator(train_data, num_steps, batch_size, vocabulary,
+                                           skip_step=num_steps)
+valid_data_generator = KerasBatchGenerator(valid_data, num_steps, batch_size, vocabulary,
+                                           skip_step=num_steps)
+
+hidden_size = 500
+use_dropout=True
+model = Sequential()
+model.add(Embedding(vocabulary, hidden_size, input_length=num_steps))
+#model.add(LSTM(hidden_size, return_sequences=True))
+#model.add(LSTM(hidden_size, return_sequences=True))
+#model.add(keras.layers.LSTM(hidden_size ,activation=tf_sqnl,recurrent_activation=tf_sqnlsig,return_sequences=True))
+#model.add(keras.layers.LSTM(hidden_size ,activation=tf_sqnl,recurrent_activation=tf_sqnlsig,return_sequences=True))
+
+
+model.add(keras.layers.LSTM(hidden_size ,activation=tf_tansig,recurrent_activation=tf_logsig,return_sequences=True))
+model.add(keras.layers.LSTM(hidden_size ,activation=tf_tansig,recurrent_activation=tf_logsig,return_sequences=True))
+
+if use_dropout:
+    model.add(Dropout(0.5))
+model.add(TimeDistributed(Dense(vocabulary)))
+model.add(Activation('softmax'))
+
+optimizer = Adam()
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
+
+print(model.summary())
+checkpointer = ModelCheckpoint(filepath=data_path + '/model-{epoch:02d}.hdf5', verbose=1)
+num_epochs = 1
+if args.run_opt == 1:
+    model.fit_generator(train_data_generator.generate(), len(train_data)//(batch_size*num_steps), num_epochs,
+                        validation_data=valid_data_generator.generate(),
+                        validation_steps=len(valid_data)//(batch_size*num_steps), callbacks=[checkpointer])
+    # model.fit_generator(train_data_generator.generate(), 2000, num_epochs,
+    #                     validation_data=valid_data_generator.generate(),
+    #                     validation_steps=10)
+    model.save(data_path + "final_model.hdf5")
+elif args.run_opt == 2:
+    model = load_model(data_path + "\model-40.hdf5")
+    dummy_iters = 40
+    example_training_generator = KerasBatchGenerator(train_data, num_steps, 1, vocabulary,
+                                                     skip_step=1)
+    print("Training data:")
+    for i in range(dummy_iters):
+        dummy = next(example_training_generator.generate())
+    num_predict = 10
+    true_print_out = "Actual words: "
+    pred_print_out = "Predicted words: "
+    for i in range(num_predict):
+        data = next(example_training_generator.generate())
+        prediction = model.predict(data[0])
+        predict_word = np.argmax(prediction[:, num_steps-1, :])
+        true_print_out += reversed_dictionary[train_data[num_steps + dummy_iters + i]] + " "
+        pred_print_out += reversed_dictionary[predict_word] + " "
+    print(true_print_out)
+    print(pred_print_out)
+    # test data set
+    dummy_iters = 40
+    example_test_generator = KerasBatchGenerator(test_data, num_steps, 1, vocabulary,
+                                                     skip_step=1)
+    print("Test data:")
+    for i in range(dummy_iters):
+        dummy = next(example_test_generator.generate())
+    num_predict = 10
+    true_print_out = "Actual words: "
+    pred_print_out = "Predicted words: "
+    for i in range(num_predict):
+        data = next(example_test_generator.generate())
+        prediction = model.predict(data[0])
+        predict_word = np.argmax(prediction[:, num_steps - 1, :])
+        true_print_out += reversed_dictionary[test_data[num_steps + dummy_iters + i]] + " "
+        pred_print_out += reversed_dictionary[predict_word] + " "
+    print(true_print_out)
+    print(pred_print_out)
